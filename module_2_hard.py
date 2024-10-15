f96943899c99cf3e55299d98b609087fbe1b0b3e
s.polishchuk <kpsserg@gmail.com>
2024-10-16 00:51:34 +0300

    module_2_hard.py - сделал домашку

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,3 +1,4 @@
+#Функция определяет верхнюю границу для внешнего цикла FOR
 def determine_counter(i):
     if i % 2:
         counter = round(i / 2) + 1
@@ -5,10 +6,10 @@ def determine_counter(i):
         counter = round(i / 2) # Привели к типу int() c помощью round()
     return counter
 
-# Функция определяет код:
+# Функция определяет шифр:
 def variations(i, n):
     global result
-    # Верхняя/нижняя граница цикла For
+    # Верхняя/нижняя граница цикла FOR
     up_border = n - i + 1
     down_border = i + 1
     #Сам цикл for
@@ -19,7 +20,7 @@ def variations(i, n):
         else:
             result = result + str(i) + str(j)
 
-n = int(input("Введите число от 3 до 20: "))
+n = int(input("Введите число от 3 до 20 \n или свой год рождения: "))
 counter = determine_counter(n)
 #print(counter)
 result = ''
@@ -27,9 +28,3 @@ for i in range(1, counter):
     variations(i, n)
 print(f'Пароль к числу {n} - {result} \n')
 
-n = int(input("А теперь введите свой год рождения: "))
-counter = determine_counter(n)
-result = ''
-for i in range(1, counter):
-    variations(i, n)
-print(f'Пароль к Вашему году рождения {n} - {result}')
